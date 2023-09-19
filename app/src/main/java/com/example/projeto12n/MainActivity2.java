1a51e77f7fd8fbe8a024a5089bf43675448804df
Eugênio Akihiro Nassu <eanassu@gmail.com>
2023-09-18 21:31:40 -0300

    Aula 18/9

---
+++ app/src/main/java/com/example/projeto12n/MainActivity2.java
@@ -0,0 +1,21 @@
+package com.example.projeto12n;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    private TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        textView = findViewById(R.id.textView);
+        textView.setText(Integer.toString(getIntent().getIntExtra("soma",0)));
+    }
+
+ }
