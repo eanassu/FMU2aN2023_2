2de08353c61b9327a7e3b57766c59c4cc1d19244
Eugênio Akihiro Nassu <eanassu@gmail.com>
2023-10-16 20:33:22 -0300

    Aula 16/10 - banco de dados local

---
+++ app/src/main/java/com/example/projeto12n/DateConverter.java
@@ -0,0 +1,13 @@
+package com.example.projeto12n;
+import androidx.room.TypeConverter;
+import java.util.Date;
+public class DateConverter {
+    @TypeConverter
+    public static Date toDate(Long timestamp) {
+        return timestamp==null?null:new Date(timestamp);
+    }
+    @TypeConverter
+    public static Long toTimestamp(Date date) {
+        return date==null?null: date.getTime();
+    }
+}
