dc909eb28cbcdedc496b74c2d3a89237aa042d0d
Eugênio Akihiro Nassu <eanassu@gmail.com>
2023-10-23 20:04:02 -0300

    Aula 23/10 - tela de cadastro

--- app/src/main/java/com/example/projeto12n/DatabaseActivity.java
+++ app/src/main/java/com/example/projeto12n/DatabaseActivity.java
@@ -3,18 +3,45 @@ package com.example.projeto12n;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class DatabaseActivity extends AppCompatActivity {
-
+    private EditText editTextRe;
+    private EditText editTextNome;
+    private EditText editTextDataAdmissao;
+    private EditText editTextSalario;
+    private EditText editTextCargo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_database);
+        editTextRe = findViewById(R.id.editTextRe);
+        editTextNome = findViewById(R.id.editTextNome);
+        editTextDataAdmissao = findViewById(R.id.editTextDataAdmissao);
+        editTextSalario = findViewById(R.id.editTextSalario);
+        editTextCargo = findViewById(R.id.editTextCargo);
+    }
+
+    public void cadastrar( View view ) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
         AppDatabase db = AppDatabase.getInstance(this);
         FuncionarioDao dao = db.funcionarioDao();
-        Funcionario f = new Funcionario(1,"teste",new Date(),1000,"cargo");
+        int re = Integer.parseInt(editTextRe.getText().toString());
+        String nome = editTextNome.getText().toString();
+        Date dataAdmissao = new Date();
+        try {
+            dataAdmissao = dateFormat.parse(editTextDataAdmissao.getText().toString());
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+        double salario = Double.parseDouble(editTextSalario.getText().toString());
+        String cargo = editTextCargo.getText().toString();
+        Funcionario f = new Funcionario(re, nome, dataAdmissao, salario, cargo);
         dao.insert(f);
     }
 }
