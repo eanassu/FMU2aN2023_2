08c12b07642710b9632bb93b3bf5fc9ad90a73c1
Eugênio Akihiro Nassu <eanassu@gmail.com>
2023-11-06 20:48:59 -0300

    Aula 30/10 - conexão HTTP

--- app/src/main/java/com/example/projeto12n/MainActivity.java
+++ app/src/main/java/com/example/projeto12n/MainActivity.java
@@ -59,4 +59,9 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, SensorActivity.class);
         startActivity(intent);
     }
+
+    public void abrirHttp( View view ) {
+        Intent intent = new Intent(this, HttpActivity.class);
+        startActivity(intent);
+    }
 }
